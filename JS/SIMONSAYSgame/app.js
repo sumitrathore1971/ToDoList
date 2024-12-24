81e79bd6df021868dc4c51114f1cca6ae41f034f
sumitrathore1971 <workwithsumit1971@gmail.com>
2024-12-24 08:47:37 -0800

    SimonSaysGame

---
+++ JS/SIMONSAYSgame/app.js
@@ -0,0 +1,90 @@
+let gameSeq = [];
+let userSeq = [];
+
+let btns =["red","yellow","green","purple"];
+
+let started = false;
+let level = 0;
+
+let h2 = document.querySelector("h2");
+let h3 = document.querySelector("h3");
+
+
+document.addEventListener("keypress",function(){
+    if (started == false) {
+        console.log("game is started");
+        started = true;
+        
+        levelup();
+    } 
+});
+
+function gameFlash(btn) {
+    btn.classList.add("flash");
+    setTimeout(function () {
+        btn.classList.remove("flash");
+    },250);
+}
+
+function userFlash(btn) {
+    btn.classList.add("greenflash");
+    setTimeout(function () {
+        btn.classList.remove("greenflash");
+    },250);
+}
+
+function levelup() {
+    userSeq = [];
+    h3.innerText = `Highest Score : ${level}`;
+    level++;
+    h2.innerText = `Level ${level}`;
+    // h3.innerText = `Highest Score : ${level}`;
+
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
+    gameFlash(randbtn);
+}
+
+function checkAns(idx) {
+    // console.log(`curr level ${level}`);
+
+
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length == gameSeq.length) {
+            setTimeout(levelup,1000);
+        }
+    } else{
+        h2.innerHTML = `Game Over! Your score war <b>${level}</b> <br> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(() => {
+            document.querySelector("body").style.backgroundColor = "white";
+        }, 150);
+        reset();
+    }
+}
+
+function btnPress(){
+    let btn = this; 
+    userFlash(btn);  
+
+    userColor = btn.getAttribute("id");
+    userSeq.push(userColor);
+
+    checkAns(userSeq.length-1);
+}
+
+let allBtns = document.querySelectorAll(".btn");
+for (btn of allBtns) {
+    btn.addEventListener("click",btnPress);
+}
+
+
+function reset(){
+    started = false;
+    gameSeq = [];
+    userSeq = [];
+    level = 0;
+}
